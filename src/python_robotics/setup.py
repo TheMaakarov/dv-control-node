cc2a4276dc106c6a2ee45606ab041a70edb59d86
TheMaakarov <the.maakrov@gmail.com>
2025-05-16 19:51:46 +0200

    feat: [TFG-2] add PythonRobotics repository fork as ROS package

---
+++ src/python_robotics/setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='python_robotics',
+    version='1.0.0',
+    packages=find_packages(),
+    install_requires=[],
+    maintainer='Iago Muñoz Varela',
+    maintainer_email='imvarela17@esei.uvigo.es',
+    description="""
+    The Python Robotics package.
+    It exposes several functionalities through a submodule.
+    """,
+    license='BSD-2.0',
+)
