37ef9290c86c3c233c04212ac7e600b28a48ffd5
TheMaakarov <the.maakarov@gmail.com>
2025-05-23 20:03:24 +0200

    feat: [TFG-2] integrate PythonRobotics modules and requirements correctly

--- src/python_robotics/setup.py
+++ src/python_robotics/setup.py
@@ -1,15 +1,23 @@
 from setuptools import find_packages, setup
+from pathlib import Path
+
+# Leer los requisitos de runtime
+req_file = Path(__file__).parent / "PythonRobotics" / "requirements" / "runtime.txt"
+requirements = req_file.read_text().splitlines()
+
+package_name = 'python_robotics'
 
 setup(
-    name='python_robotics',
+    name=package_name,
     version='1.0.0',
-    packages=find_packages(),
-    install_requires=[],
+    packages=find_packages(exclude=['tests*']),
+    install_requires=['setuptools'] + requirements,
+    zip_safe=True,
     maintainer='Iago Muñoz Varela',
     maintainer_email='imvarela17@esei.uvigo.es',
     description="""
     The Python Robotics package.
     It exposes several functionalities through a submodule.
     """,
-    license='BSD-2.0',
+    license='MIT',
 )
